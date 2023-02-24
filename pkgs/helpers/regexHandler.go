f0d69d1355149ac76ccbdf02a37a54650bdb9ca1
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-02-24 21:10:17 +0300

    feat:(added regex handler)

--- pkgs/helpers/regexHandler.go
+++ pkgs/helpers/regexHandler.go
@@ -1,6 +1,8 @@
 package regexHandler
 
-import "regexp"
+import (
+	"regexp"
+)
 
 func GetSubString(s string, index []int) string {
 	if index == nil {
@@ -9,8 +11,20 @@ func GetSubString(s string, index []int) string {
 	return s[index[0]:index[1]]
 }
 
-func RegExp(link string) string {
-	pattern := regexp.MustCompile(`[a-z]*@[a-z]*\.(com|org)`)
+func MatchCompanyLink(link string) string {
+	pattern := regexp.MustCompile("https://www.mukwano.com/")
+	matchedIndex := pattern.FindStringIndex(link)
+	return GetSubString(link, matchedIndex)
+}
+
+func MatchEmail(link string) string {
+	pattern := regexp.MustCompile(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`)
+	matchedIndex := pattern.FindStringIndex(link)
+	return GetSubString(link, matchedIndex)
+}
+
+func MatchContactUs(link string) string {
+	pattern := regexp.MustCompile("https://www.creec.or.ug/contact-us-2/")
 	matchedIndex := pattern.FindStringIndex(link)
 	return GetSubString(link, matchedIndex)
 }
