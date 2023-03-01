402ccc6b4ccecad51ef42c01134c995b1a4fe7de
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-03-01 07:57:59 +0300

    feat:(added tests for the scrapper functions)

---
+++ pkgs/scrappers/scrapper_test.go
@@ -0,0 +1,37 @@
+package scrapper
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGoogleScrapper(t *testing.T) {
+	got := GoogleScrapper("mukwano")
+	want := "https://www.mukwano.com/"
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("Expected: %s got: %s", want, got)
+	}
+}
+
+func TestCompanyScrapper(t *testing.T) {
+	type test struct {
+		companyLink  string
+		companyName  string
+		companyEmail string
+	}
+	tests := []test{
+		{"https://www.mukwano.com/", "mukwano", "customercare@mukwano.com"},
+		{"https://www.mukwano.com/", "mukwano", "customercare@mukwano.com"},
+	}
+	for _, tc := range tests {
+		email, name := CompanyScrapper(tc.companyLink, tc.companyName)
+		if email != tc.companyEmail || name != tc.companyName {
+			t.Fatalf("Expected: Email: %v Name: %v, Got: Email: %v Name: %v", tc.companyEmail, tc.companyName, email, name)
+		}
+	}
+
+}
+
+// func TestContactUsScrapper(t *testing.T){
+// 	type test struct
+// }
