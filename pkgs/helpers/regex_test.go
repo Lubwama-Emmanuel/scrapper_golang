c25542c62c63eee934ff4122c6f5666ec6581859
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-03-07 10:18:54 +0300

    feat:(added test name)

--- pkgs/helpers/regex_test.go
+++ pkgs/helpers/regex_test.go
@@ -9,20 +9,21 @@ import (
 func TestMatchCompanyLink(t *testing.T) {
 	t.Parallel()
 	type test struct {
+		testName    string
 		link        string
 		companyLink string
 		companyName string
 	}
 	tests := []test{
-		{"/url?q=https://www.mukwano.com/&sa=U&ved=2ah", "https://www.mukwano.com", "mukwano"},
-		{"/url?q=http://www.mukwano.com/&sa=U&ved=2ahUK", "http://www.mukwano.com", "mukwano"},
+		{"Test link with www", "/url?q=https://www.mukwano.com/&sa=U&ved=2ah", "https://www.mukwano.com", "mukwano"},
+		{"Test link without www", "/url?q=http://codebits.io/&sa=U&ved=2ahU", "http://codebits.io", "codebits"},
 	}
 
 	for i := range tests {
 		got := regexhandler.MatchCompanyLink(tests[i].link, tests[i].companyName)
 
 		if got != tests[i].companyLink {
-			t.Fatalf("Expected: %v But Got: %v", tests[i].companyLink, got)
+			t.Fatalf("%v: Expected: %v But Got: %v", tests[i].testName, tests[i].companyLink, got)
 		}
 	}
 }
@@ -30,18 +31,20 @@ func TestMatchCompanyLink(t *testing.T) {
 func TestMatchEmail(t *testing.T) {
 	t.Parallel()
 	type test struct {
-		link string
-		want string
+		testName string
+		link     string
+		want     string
 	}
 	tests := []test{
-		{"customercare@mukwano.com", "customercare@mukwano.com"},
+		{"Test email with .com", "customercare@mukwano.com", "customercare@mukwano.com"},
+		{"Test email without .com", "mailto:hello@codebits.io", "hello@codebits.io"},
 	}
 
 	for i := range tests {
 		got := regexhandler.MatchEmail(tests[i].link)
 
 		if got != tests[i].want {
-			t.Fatalf("Expected: %v But Got: %v", tests[i].want, got)
+			t.Fatalf("%v: Expected: %v But Got: %v", tests[i].testName, tests[i].want, got)
 		}
 	}
 }
