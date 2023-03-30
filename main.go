793413ca6c8174a0c11b57736de397b6f8868f43
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-03-30 11:27:54 +0300

    fix:(parsed google search link from main)

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	collectionMap := make(map[string]string)
-	const googleSearchUrl = "https://www.google.com/search?q=%s"
+	const googleSearchURL = "https://www.google.com/search?q=%s"
 
 	companies, err := scrapper.ReadFromFile("uploadedFiles/company_list-4096951222.txt")
 	if err != nil {
@@ -21,7 +21,7 @@ func main() {
 			companies[i] = strings.ReplaceAll(companies[i], " ", "")
 		}
 
-		companyLink, err := scrapper.GoogleScrapper(googleSearchUrl, companies[i])
+		companyLink, err := scrapper.GoogleScrapper(googleSearchURL, companies[i])
 		if err != nil {
 			log.Error(err)
 		}
